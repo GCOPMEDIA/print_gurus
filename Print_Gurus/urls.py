9b3207988b7ef167523cd0f9866fdd2a4f160b2e
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-21 22:35:14 +0000

    app setup

--- Print_Gurus/urls.py
+++ Print_Gurus/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import about
+from .views import *
+
 
 urlpatterns = [
-    path('about/', about,name='about'),
+    path('all-posts/', all_posts,name='all_posts'),
+    path('about/',about,name='about'),
+path('like-post/<int:post_id>/', like_post),
+    path("csrf/",get_csrf)
 
 ]
