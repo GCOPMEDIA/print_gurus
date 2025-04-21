9007753d81dbd5d0ccc3414c1647f9f7429fca40
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-21 22:34:45 +0000

    app setup

--- Print_Gurus/admin.py
+++ Print_Gurus/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(AboutPageBackground)
+admin.site.register(Like)
+admin.site.register(Leaders)
+admin.site.register(AuthUser)
