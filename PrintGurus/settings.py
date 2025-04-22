f3c2994b56f3a5b1efcdde4c7cf5f915d836dd30
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-22 12:46:11 +0000

    app setup

--- PrintGurus/settings.py
+++ PrintGurus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4f(cdh+3x8^@#-9yjr@@c)v6c636%2$*(km%0)n(q$0w2&1nmd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["print-gurus.onrender.com"]
+ALLOWED_HOSTS = ["print-gurus.onrender.com",'127.0.0.1:8000']
 
 
 # Application definition
