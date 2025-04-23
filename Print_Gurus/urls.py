7c6481dc4ab0878d9067b08f4b36e22787bb5767
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-23 22:21:52 +0000

    app setup

--- Print_Gurus/urls.py
+++ Print_Gurus/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from .views import *
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+router = DefaultRouter()
+router.register(r'comments', CommentViewSet, basename='comment')
 
 
 urlpatterns = [
@@ -14,6 +17,9 @@ path('like-post/<int:post_id>/', like_post),
     path("csrf/",get_csrf),
 path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/signup',signup_view)
+    path('api/signup',signup_view),
+    path('', include(router.urls)),
+    path('check/',check_auth,name='check')
+
 
 ]
