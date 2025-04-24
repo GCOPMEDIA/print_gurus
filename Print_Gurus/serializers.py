042e5b2f387ff428c3983753cdf0a6e26213ff38
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-24 22:01:22 +0000

    app setup

--- Print_Gurus/serializers.py
+++ Print_Gurus/serializers.py
@@ -1,14 +1,14 @@
-from rest_framework import serializers
-from .models import PostComments
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    author_name = serializers.CharField(source='user.username', read_only=True)
-    author_avatar = serializers.SerializerMethodField()
-
-    class Meta:
-        model = PostComments
-        fields = ['comment_id', 'post', 'comment', 'parent', 'timestamp', 'author_name', 'author_avatar']
-
-    def get_author_avatar(self, obj):
-        return getattr(obj.user.profile, 'avatar', None) if hasattr(obj.user, 'profile') else None
+# from rest_framework import serializers
+# from .models import PostComments
+#
+#
+# class CommentSerializer(serializers.ModelSerializer):
+#     author_name = serializers.CharField(source='user.username', read_only=True)
+#     author_avatar = serializers.SerializerMethodField()
+#
+#     class Meta:
+#         model = PostComments
+#         fields = ['comment_id', 'post', 'comment', 'parent', 'timestamp', 'author_name', 'author_avatar']
+#
+#     def get_author_avatar(self, obj):
+#         return getattr(obj.user.profile, 'avatar', None) if hasattr(obj.user, 'profile') else None
