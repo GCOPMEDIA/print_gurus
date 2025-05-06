5ba32efc48c82c0b169b5ffe46cb70f3a7e379b8
Kobby24 <geffah005@st.ug.edu.gh>
2025-05-06 00:44:13 +0000

    model

--- PrintGurus/settings.py
+++ PrintGurus/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4f(cdh+3x8^@#-9yjr@@c)v6c636%2$*(km%0)n(q$0w2&1nmd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "print-gurus.onrender.com",
